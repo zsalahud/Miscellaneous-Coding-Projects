26b085d06d5ff13eb67215006f71a7a31dee443b
Zunaeed Salahuddin <zsalahud@iu.edu>
2019-01-16 00:39:54 -0500

    Update RRA.java

--- RRA.java
+++ RRA.java
@@ -8,22 +8,45 @@ public class RRA{
 
     int[][] matrix = new int[array.length][array[0].length];
     
-    for (int i = 0; i < matrix.length; i++)
+    // copy input matrix
+    for (int r = 0; r < matrix.length; r++)
     {
-        for (int j = 0; j < matrix[j].length; j++)
+        for (int c = 0; c < matrix[r].length; c++)
         {
-            matrix[i][j] = array[i][j];
+            matrix[r][c] = array[r][c];
         }
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
+    for (int p = 0; p < matrix.length; p++){
+       // creating left-most pivot 
+        int pivot = matrix[p][p];
+        if (pivot != 0)
+        {
+            int other_pivot = 1 / pivot;
+            for (int i = 0; i < matrix[p].length; i++)
+            {
+                matrix[p][i] *= other_pivot;
+            }
+        }
+        // make zero-rows
+        for (int r = 0; r < matrix.length; r++)
+        {
+            if (r != p)
+            {
+                int f = matrix[r][p];
+                for (int i = 0; i < matrix[r].length; i++)
+                {
+                    matrix[r][i] -= f * matrix[p][i];
+                }
+            }
+        }
+    }
+    return matrix;
   }
+  
+  public static void main(String[] args){
+    int[][] x = {{3,5,-1,10},{1,4,1,7},{9,0,2,1}};
+    System.out.println(Arrays.deepToString(row_reducer(x)));
 
+  }
 }
